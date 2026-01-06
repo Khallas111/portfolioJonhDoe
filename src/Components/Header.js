aa61a3b48e38d32a172f63ce20fe854b4c3af5df
Khallas111 <salimkhorchi11@gmail.com>
2026-01-06 18:41:02 +0100

    Logo
    Création du composant, à modifier plus tard : Importation de la modale au clic du logo.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Navigation from "../Components/Tools/Navigation";
+import Logo from "../Components/Tools/Logo";
 const Header = () => {
   return (
     <div>
+      <Logo />
       <Navigation />
     </div>
   );
