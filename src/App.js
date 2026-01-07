74e756598a09759467c2514f85075f105a332e2b
Khallas111 <salimkhorchi11@gmail.com>
2026-01-07 20:40:27 +0100

    Responsive établit sur App.js
    Le responsive a été établit sur App. Il reste à importer sur les diverses pages de l'application.

--- src/App.js
+++ src/App.js
@@ -5,19 +5,30 @@ import About from "./pages/About";
 import Achievements from "./pages/Achievements";
 import Copyrights from "./pages/Copyrights";
 import Services from "./pages/Services";
+import Container from "react-bootstrap/Container";
+
+function Layout({ children }) {
+  return (
+    <Container fluid className="px-3">
+      {children}
+    </Container>
+  );
+}
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/achievements" element={<Achievements />} />
-        <Route path="/copyrights" element={<Copyrights />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <Layout>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/achievements" element={<Achievements />} />
+          <Route path="/copyrights" element={<Copyrights />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </Layout>
   );
 };
 
--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -3,33 +3,53 @@ import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import CardForServices from "../Components/tools/cards/CardsForServices";
 import { BsBrush, BsSearch, BsCodeSlash } from "react-icons/bs";
-
+import { Container, Row, Col } from "react-bootstrap";
+import Image from "react-bootstrap/Image";
+import banner from "../assets/images/banner.jpg";
 const Services = () => {
   return (
-    <div>
+    <>
       <Header />
-      <div className="titre">
-        <h1>Mon offre de services</h1>
-        <p>Voici les prestations sur lesquelles je peux intervenir</p>
-      </div>
-      <div className="services"></div>
-      <CardForServices
-        title="UX Design"
-        text="L'UX Design est une discipline qui consiste à concevoir des produits (sites web, applivations mobiles, logiciels, objets connectés, etc.) en plaçant l'utilisateur au centre des précoccupations. L'objectif est de rendre l'expérience utilisateur la plus fluide et agréable possible."
-        icon={BsBrush}
-      />
-      <CardForServices
-        title="Développement Web"
-        text="Le développement de sites web consiste à créer des sites internet en utilisant des langages de programmation (HTML, CSS, JavaScript, PHP, etc.) et des frameworks (Bootstrap, React, Angular, etc.)."
-        icon={BsCodeSlash}
-      />
-      <CardForServices
-        title="Référencement"
-        text="Le référencement naturel (SEO) est une technique qui consiste à optimiser un site web pour le faire remonter dans les résultats des moteurs de recherche (Google, Bing, Yahoo, etc.). L'objectif est d'attirer un maximum de visiteurs qualifiés sur le site."
-        icon={BsSearch}
-      />
+      <Image src={banner} fluid alt="Bannière services" className="my-4" />
+      <Container className="titre-services">
+        {/* Titre */}
+        <Row className="text-center mb-5">
+          <Col>
+            <h1>Mon offre de services</h1>
+            <p>Voici les prestations sur lesquelles je peux intervenir</p>
+          </Col>
+        </Row>
+
+        {/* Cartes */}
+        <Row className="g-4">
+          <Col xs={12} md={6} lg={4}>
+            <CardForServices
+              title="UX Design"
+              text="L'UX Design est une discipline qui consiste à concevoir des produits (sites web, applications mobiles, logiciels, objets connectés, etc.) en plaçant l'utilisateur au centre des préoccupations. L'ojesctif est de rendre l'expérience utilisateur la plus fluide et agréable possible."
+              icon={BsBrush}
+            />
+          </Col>
+
+          <Col xs={12} md={6} lg={4}>
+            <CardForServices
+              title="Développement Web"
+              text="Le développement de sites web consiste à créer des sites internet en utilisant des langages de programmation (HTML, CSS, JavaScript, PHP, etc.) et des frameworks (Bootstrap, React, Angular, etc.)."
+              icon={BsCodeSlash}
+            />
+          </Col>
+
+          <Col xs={12} md={6} lg={4}>
+            <CardForServices
+              title="Référencement"
+              text="Le référencement (SEO) est une technique qui consiste à otpimiser un site web pour le faire remonter dans les résultats des moteurs de recherche (Google, Bing, etc.). L'objectif est d'attirer un maximum de visiteurs qualifiés sur le site."
+              icon={BsSearch}
+            />
+          </Col>
+        </Row>
+      </Container>
+
       <Footer />
-    </div>
+    </>
   );
 };
 
