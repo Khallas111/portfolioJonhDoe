64152b2563f9b1071ce5cf6dda5ad314acafa8a2
Khallas111 <salimkhorchi11@gmail.com>
2026-01-07 14:16:39 +0100

    Bootstrap
    Importation de Bootstrap et de ses dépendances dans le projet.

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Header from "../Components/Header";
+import Footer from "../Components/Footer";
 
 const Services = () => {
   return (
     <div>
       <Header />
-      <h1>Services</h1>
+      <div className="titre">
+        <h1>Mon offre de services</h1>
+        <p>Voici les prestations sur lesquelles je peux intervenir</p>
+      </div>
+      <div className="services"></div>
+      <Footer />
     </div>
   );
 };
