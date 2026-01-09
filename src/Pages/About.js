b818d586cbc6e6756d8046e4d320d3308eda0cd5
Khallas111 <salimkhorchi11@gmail.com>
2026-01-09 16:39:06 +0100

    Footer
    Import du Footer dans toutes les pages de l'App

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../Components/Header";
+import Footer from "../Components/Footer";
 
 const About = () => {
   return (
     <div>
       <Header />
       <h1>Contact</h1>
+      <Footer />
     </div>
   );
 };
