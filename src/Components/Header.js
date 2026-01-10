b7612754306cc7d3a144111f15fd85831be71e3b
Khallas111 <salimkhorchi11@gmail.com>
2026-01-10 14:18:54 +0100

    Header
    Att ! Logo cliquable!! En attente de la création de la Modale (rechargement de la page)

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,12 +2,15 @@ import React from "react";
 import Navigation from "./tools/Navigation";
 import Logo from "./tools/Logo";
 import { Container, Navbar } from "react-bootstrap";
+import "../styles/Header.scss";
 
 const Header = () => {
   return (
-    <Navbar expand="lg" className="header bg-dark">
+    <Navbar expand="lg" className="header" variant="dark">
       <Container>
-        <Logo />
+        <Navbar.Brand href="/" className="navbar-brand-custom">
+          <Logo />
+        </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="main-navbar" />
 
