c9f663d6ed119205b590c42e225c51ba6439265e
Khallas111 <salimkhorchi11@gmail.com>
2026-01-13 16:19:51 +0100

    Boutons Portfolio
    Ajout des boutons

---
+++ src/Components/tools/BoutonsPortfolio.js
@@ -0,0 +1,13 @@
+import Button from "react-bootstrap/Button";
+
+function VoirSite() {
+  return (
+    <div className="btn-portfolio-wrapper">
+      <Button variant="primary" className="btn-portfolio">
+        Voir le site
+      </Button>
+    </div>
+  );
+}
+
+export default VoirSite;
