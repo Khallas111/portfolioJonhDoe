5f470584750d4c7d7300b7ace7ba219613d93230
Khallas111 <salimkhorchi11@gmail.com>
2026-01-13 14:14:04 +0100

    Bases Pages Portfolio
    Import de Image, Container, Row et Col

--- src/Pages/Achievements.js
+++ src/Pages/Achievements.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
+import banner from "../assets/images/banner.jpg";
+import { Container, Row, Col } from "react-bootstrap";
+import Image from "react-bootstrap/Image";
 const Achievements = () => {
   return (
     <div>
       <Header />
-      <h1>Réalisations</h1>
+      {/* Image Bannière */}
+      <Image src={banner} fluid alt="Bannière services" className="mb-4" />
+      <Container className="services mb-5"></Container>
+      <h1>Portfolio</h1>
       <Footer />
     </div>
   );
